6633b223c97781c643423ed43b58361b1f5ff1e4
caozhen <2482923743@qq.com>
2022-11-22 11:29:41 +0800

    修改了login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,12 @@
 $(function () {
     // 点击 去注册账号的连接
     $("#link_reg").click(function () {
-        $(this).parent().hide();
+        $(".login-box").hide();
         $(".reg-box").show();
     });
     // 点击 去登录的连接
     $("#link_login").click(function () {
-        $(this).parent().hide();
+        $(".reg-box").hide();
         $(".login-box").show();
     });
 });
