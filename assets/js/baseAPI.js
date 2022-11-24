5b7b61d9bcec90eb6fcbe7cc9d21ba9fd6fd5724
caozhen <2482923743@qq.com>
2022-11-24 10:57:30 +0800

    完成了退出的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,10 @@
 // 每次调用 $.get 或 $.post() $.ajax() 的时候会自动调用这个方法
 $.ajaxPrefilter(function (options) {
     // console.log(options);
-    // options.url
+    // options.url = ''
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,21 @@
 var url = 'http://www.liulongbin.top:3007'
 $(function () {
     getUserInfo();
+    // 点击按钮实现点击退出功能
+    $('#btnlogout').on('click', function () {
+        // 提示用户是否确认退出
+        layui.layer.confirm('是否退出?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2.重新跳转到登录首页
+            location.href = './login.html'
+            // 关闭询问框
+            layui.layer.close(index)
+        });
+    });
 });
 
 // 获取用户的基本信息
@@ -9,9 +24,9 @@ function getUserInfo() {
         method: 'get',
         url: `${url}/my/userinfo`,
         // 请求头配置对象
-        headers: {
-            Authorization: localStorage.getItem('token') || ""
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ""
+        // },
         success: function (resp) {
             if (resp.status !== 0) {
                 return layui.layer.msg('获取用户信息失败!')
@@ -30,11 +45,13 @@ function renderAvatar(user) {
     // 3.按需渲染用户的头像
     if (user.user_pic !== null) {
         // 渲染图片头像
-        $(".layui-nav-img").attr('src',user.user_pic).show();
+        $(".layui-nav-img").attr('src', user.user_pic).show();
         $('.text-avatar').hide();
     } else {
         // 渲染文本头像
         $(".layui-nav-img").hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
     }
 
 }
